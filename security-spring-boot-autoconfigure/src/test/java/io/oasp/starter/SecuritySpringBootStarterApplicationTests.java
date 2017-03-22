4140b259f2feabd23c1fa2d5bf5774da85c23f4e
Rafal Jozwik <rafal.jozwik@capgemini.com>
2017-03-22 16:56:36 +0100

    Structure changed

--- security-spring-boot-autoconfigure/src/test/java/io/oasp/starter/SecuritySpringBootStarterApplicationTests.java
+++ security-spring-boot-autoconfigure/src/test/java/io/oasp/starter/SecuritySpringBootStarterApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SecurityApplicationTests {
+public class SecuritySpringBootStarterApplicationTests {
 
 	@Test
 	public void contextLoads() {
